5ef349157d0263f294b9ebaf7f52a68b7b357da3
kontb2a22 <60299751+kontb2a22@users.noreply.github.com>
2022-07-28 15:34:18 +0200

    Update index.js

--- Solana-NFT/src/index.js
+++ Solana-NFT/src/index.js
@@ -9,9 +9,7 @@ import { MoralisProvider } from "react-moralis";
 
 ReactDOM.render(
   <React.StrictMode>
-    <MoralisProvider appId="9EXwkE38PWFbGTZZKvkj3Gt8cHfkctUU3JWdBtmm" serverUrl="https://sctfgyfs60bu.usemoralis.com:2053/server
-
-">
+    <MoralisProvider appId="9EXwkE38PWFbGTZZKvkj3Gt8cHfkctUU3JWdBtmm" serverUrl="https://sctfgyfs60bu.usemoralis.com:2053/server">
           <App />
     </MoralisProvider>
   </React.StrictMode>,
